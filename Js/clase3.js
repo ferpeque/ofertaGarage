5e969e0fd72e553dfb3fb970499a5920815606eb
fer peque <fernandopequeno@gmail.com>
2021-04-16 00:15:17 -0300

    3.2

--- Js/clase3.js
+++ Js/clase3.js
@@ -1,1 +1,35 @@
 console.log("Array&Objects")
+
+
+// creo una clase/objeto producto 
+
+class tarjetaProducto {
+    constructor (nombre, precio, id) {
+        this.nombre = nombre
+        this.precio = precio
+        this.id = id
+    }
+}
+
+
+const carrito = []
+
+// va a recibir el array
+let agregarProductos = () => {
+    return carrito
+}
+
+
+
+// va a crear los objetos que se van a agregar al carrito
+let crearProducto = (producto) => {
+    carrito.push(producto)
+
+}
+
+const producto1 = new tarjetaProducto("Mini componente", 800, 1)
+const producto2 = new tarjetaProducto("Canon D 60x", 1000, 1)
+
+crearProducto (producto1)
+crearProducto (producto2)
+console.log(agregarProductos())
