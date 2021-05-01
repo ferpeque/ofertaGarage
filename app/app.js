98b544f3dca3bb0ffc0c9b1d919f31f4263bd863
fer peque <fernandopequeno@gmail.com>
2021-05-01 20:05:07 -0300

    1.3

--- app/app.js
+++ app/app.js
@@ -1,7 +1,4 @@
 
-        //function oferta () {
-        //    console.log("ingrese su oferta")
-        //}
 
 
 // Recupero estructura hmtl del producto
@@ -10,32 +7,97 @@ let contenedorTarjeta = document.getElementById("container-product")
 
 // forEach recorre todo el Array
 productos.forEach(product => {
-    console.log (product)
+        //console.log (product)
 
 //Creo tarjeta producto.
 //Por cada product me crea un html 
 let div = document.createElement("div")
 div.classList.add("card")
-console.log(div)
+        //console.log(div)
 
 div.innerHTML = `
 
     <img src=${product.img} alt="" class="card__img">
     <h4 class="card__title">${product.nombre}</h4>
-    <p class="card__description">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim quaerat ut
-    debitis. Animi reprehenderit quis similique expedita! Suscipit, deleniti eos!</p>
-    <input type="text" id="input-oferta" name="Oferta1" placeholder="Ingrese su Oferta" id="Oferta1" class="imput__textera">
-    <a href="#" id="card1" class="card__btn">Leer más</a>
+    <p class="card__description">${product.desc}</p>
+    <input type="text" id="input-oferta" name="Oferta1" placeholder="Ingrese su Oferta" class="imput__textera">
+    <a href="#" id="card1" onclick=ofertaUser(event) class="card__btn">Ofertar</a>
     <div id="resultado"
-
+    <a href="#" id="AgregarCarrito" onclick=agregarCarrito(${product.id}) class="card__btn">AgregarCarrito</a>
 `
 contenedorTarjeta.appendChild(div)
+
 })   
 
-const tarjetaProducto = document.getElementById("card1")
-let ofertaIngresada = document.getElementById("input-oferta") 
-let mensajeOferta = document.getElementById("resultado") 
 
-//console.log (tarjetaProducto)
-// console.log (ofertaIngresada)
 
+
+function ofertaUser (event) {
+        // console.log (event.target)
+
+// previene actualizacion de la pagima
+    event.preventDefault()
+   
+    let ofertaIngresada = document.getElementById("input-oferta") 
+    console.log(ofertaIngresada)
+    
+    let ofertaUser = parseInt(ofertaIngresada.value)
+    console.log(ofertaUser) 
+    console.log(typeof ofertaUser)
+    
+/*
+
+
+    let mensajeOferta = document.getElementById("resultado") 
+     // muestro resultado del User y agrego boton carrito
+        let itemOferta = document.createElement('p')
+        itemOferta.textContent = `La oferta ingresada es  ${ofertaUser} `
+        itemOferta.innerHTML = `
+        <button onclick=agregarAlCarrito() class="btn" type="submit"> Agregar carrito </button> `
+        mensajeOferta.appendChild(itemOferta) 
+
+*/    
+}
+
+//
+
+
+//BOTON
+function agregarCarrito (id) {
+    //recuperar el elemento elegito al clickear
+    const productoElegido = productos.find( el => el.id == id) 
+     //console.log(productoElegido)
+
+    // si elegiste, sumar al array del carrito. 
+     if (productoElegido) {
+        carrito.push(productoElegido)
+     }
+
+     productosSeleccionados ()
+
+     console.log(carrito)   
+    
+}
+
+//AGREGRAR PRODUCTOS SELECCIONADOS A MODAL
+
+function productosSeleccionados () {
+    const contenedorCarrito = document.getElementById("carrito-contenedor")
+    
+    // vaciar el carrito 
+    contenedorCarrito.innerHTML=""
+
+    carrito.forEach((product) => {
+        contenedorCarrito.innerHTML += `
+        <div class="productoEnCarrito">
+        <p>${product.nombre}</p>
+        <p>Precio: $1200</p>
+        <button class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+    </div>`
+    })
+
+}
+
+
+
+carrito = []
--- app/productos.js
+++ app/productos.js
@@ -1,8 +1,34 @@
 // Array de Objetos
+
 let productos = [
-    {id: 1, nombre: "Card1", img: "Img/Canon__Eos__RebelxT.jpg", },     //oferta: oferta()
-    {id: 2, nombre: "Card2", img: "Img/EquipoAudio__Philips.jpg", },    //oferta: oferta()
-    {id: 3, nombre: "Card3", img: "Img/Fmd_System.jpg", },              //oferta: oferta()
-    {id: 4, nombre: "Card4", img: "Img/FujiFilm__instax100.jpg", },     //oferta: oferta()
+
+    {id:        1, 
+    nombre:     "Card1", 
+    img:        "Img/Canon__Eos__RebelxT.jpg", 
+    desc:       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim quaerat utdebitis. Animi reprehenderit quis similique expedita! Suscipit, deleniti eos!",
+    oferta:     ""
+},    
+
+    {id:        2, 
+    nombre:     "Card2", 
+    img:        "Img/EquipoAudio__Philips.jpg",
+    desc:       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim quaerat utdebitis. Animi reprehenderit quis similique expedita! Suscipit, deleniti eos!",
+    oferta:     ""
+},   
+
+    {id:        3, 
+    nombre:     "Card3", 
+    img:        "Img/Fmd_System.jpg",
+    desc:       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim quaerat utdebitis. Animi reprehenderit quis similique expedita! Suscipit, deleniti eos!",
+    oferta:     ""
+    },  
+
+    {id:        4, 
+    nombre:     "Card4",
+    img:        "Img/FujiFilm__instax100.jpg",
+    desc:       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim quaerat utdebitis. Animi reprehenderit quis similique expedita! Suscipit, deleniti eos!",
+    oferta:     ""
+},     
 ]
+
 
